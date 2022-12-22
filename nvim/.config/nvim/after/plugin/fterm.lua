485616b5904493a8e48549029ef82f055d0982d9
Noah Shreve <noah.shreve@closedloop.ai>
2022-12-22 09:55:29 -0500

    fterm toggle fix

--- nvim/.config/nvim/after/plugin/fterm.lua
+++ nvim/.config/nvim/after/plugin/fterm.lua
@@ -1,2 +1,2 @@
 vim.keymap.set("n", "<A-t>", "<CMD>lua require('FTerm').toggle()<CR>")
-vim.keymap.set("t", "<A-t>", "<C-//><C-n><CMD>lua require('FTerm').toggle()<CR>")
+vim.keymap.set("t", "<A-t>", "<C-n><CMD>lua require('FTerm').toggle()<CR>")
