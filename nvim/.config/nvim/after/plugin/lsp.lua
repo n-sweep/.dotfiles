c7281f04386729852264a782930faa3426e99d1b
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2022-12-26 10:14:39 -0500

    tab completion behavior

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,46 @@
 local lsp = require("lsp-zero")
+local cmp = require("cmp")
+
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line-1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    -- Scroll docs
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    -- Tab scrolls autocomplete options
+    ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+            cmp.complete()
+        else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
+        end
+    end, { "i", "s" })
+})
+
 lsp.preset("recommended")
 
 lsp.ensure_installed({
     'jedi_language_server'
 })
-
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
 lsp.nvim_workspace()
 lsp.setup()
 
