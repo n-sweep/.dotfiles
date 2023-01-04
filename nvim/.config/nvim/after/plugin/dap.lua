fe366c08a694f907d63dcd17a2355c2905f5b61c
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-01-04 08:07:06 -0500

    python DAP

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -5,13 +5,24 @@ local dap_vt = require('nvim-dap-virtual-text')
 local dap_python = require('dap-python')
 
 vim.keymap.set('n', '<F5>', function() dap.continue() end)
-vim.keymap.set('n', '<F3>', function() dap.step_into() end)
-vim.keymap.set('n', '<F4>', function() dap.step_over() end)
-vim.keymap.set('n', '<F6>', function() dap.step_out() end)
+vim.keymap.set('n', '<F9>', function() dap.step_into() end)
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+vim.keymap.set('n', '<F11>', function() dap.step_out() end)
+vim.keymap.set('n', '<F12>', function() dap.terminate() end)
 vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<leader>B', function() dap.set_breakpoint('Breakpoint Condition: ') end)
 vim.keymap.set('n', '<leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log Point Message: ')) end)
-vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)
+vim.keymap.set('n', '<leader>du', function() dapui.toggle() end)
+
+dap.listeners.after.event_initialized['dapui_config'] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+    dapui.close()
+end
 
 dapui.setup()
 dap_vt.setup()
