4ab28144db67052522aa67c306ba6da9e5d0ee63
Noah Shreve <noah.shreve@closedloop.ai>
2023-01-13 14:12:11 -0500

    what is up with alt on linux vs windows

--- nvim/.config/nvim/after/plugin/tmux.lua
+++ nvim/.config/nvim/after/plugin/tmux.lua
@@ -1,15 +1,15 @@
 local vim = vim
 local tmux = require("tmux")
 
-vim.keymap.set("n", "<A-h>", function() tmux.move_left() end)
-vim.keymap.set("n", "<A-j>", function() tmux.move_bottom() end)
-vim.keymap.set("n", "<A-k>", function() tmux.move_top() end)
-vim.keymap.set("n", "<A-l>", function() tmux.move_right() end)
+vim.keymap.set("n", "<M-h>", function() tmux.move_left() end)
+vim.keymap.set("n", "<M-j>", function() tmux.move_bottom() end)
+vim.keymap.set("n", "<M-k>", function() tmux.move_top() end)
+vim.keymap.set("n", "<M-l>", function() tmux.move_right() end)
 
-vim.keymap.set("n", "<C-A-h>", function() tmux.resize_left() end)
-vim.keymap.set("n", "<C-A-j>", function() tmux.resize_bottom() end)
-vim.keymap.set("n", "<C-A-k>", function() tmux.resize_top() end)
-vim.keymap.set("n", "<C-A-l>", function() tmux.resize_right() end)
+vim.keymap.set("n", "<C-M-h>", function() tmux.resize_left() end)
+vim.keymap.set("n", "<C-M-j>", function() tmux.resize_bottom() end)
+vim.keymap.set("n", "<C-M-k>", function() tmux.resize_top() end)
+vim.keymap.set("n", "<C-M-l>", function() tmux.resize_right() end)
 
 tmux.setup({
     copy_sync = {enable = false},
