81b82b2e862db05283f596e2cc887bafb67b8be1
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-08-21 09:22:15 -0400

    mappings

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -11,8 +11,11 @@ vim.keymap.set('n', '<F11>', function() dap.step_out() end)
 vim.keymap.set('n', '<F12>', function() dap.terminate() end)
 vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<leader>B', function() dap.set_breakpoint('Breakpoint Condition: ') end)
-vim.keymap.set('n', '<leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log Point Message: ')) end)
 vim.keymap.set('n', '<leader>du', function() dapui.toggle() end)
+
+-- I need <leader>l
+-- maybe this plugin just needs removed, I don't use it.
+--vim.keymap.set('n', '<leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log Point Message: ')) end)
 
 dap.listeners.after.event_initialized['dapui_config'] = function()
     dapui.open()
--- nvim/.config/nvim/after/plugin/markdown-preview.lua
+++ nvim/.config/nvim/after/plugin/markdown-preview.lua
@@ -1,8 +1,12 @@
 local vim = vim
+local g = vim.g
 
-vim.g.vim_markdown_folding_disabled = 1
-vim.g.vim_markdown_conceal = 0
-vim.g.vim_markdown_conceal_code_blocks = 0
-vim.g.vim_markdown_formatter = 1
+g.vim_markdown_folding_disabled = 1
+g.vim_markdown_conceal = 0
+g.vim_markdown_conceal_code_blocks = 0
+g.vim_markdown_formatter = 1
+
+g.mkdp_auto_start=1
+g.mkdp_auto_close=0
 
 vim.keymap.set('n', '<leader>mp', ":MarkdownPreview<CR>")
