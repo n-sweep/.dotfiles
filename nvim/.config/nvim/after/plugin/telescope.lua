d6a077a41590729229d6d5c25f61fba694d50dae
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-10-01 06:57:12 -0400

    making find_files recognize hidden files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
--- open file_browser when opening neovim / netrw
+local vim = vim
+
 require('telescope').setup{
     defaults = { sorting_strategy = 'ascending'},
     extensions = {
@@ -13,10 +14,14 @@ require('telescope').load_extension 'file_browser'
 
 -- keymappings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>n', ':Telescope file_browser path=%:p:h <CR>')
+local function ffh() builtin.find_files({hidden=true}) end
+
+vim.keymap.set('n', '<leader>ff', ffh, {})  -- I hate that you have to do this
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>dd', builtin.lsp_definitions, {})
+
+vim.keymap.set('n', '<leader>n', ':Telescope file_browser path=%:p:h <CR>')
+vim.keymap.set('n', '<leader>ft', ":Telescope find_files hidden=true<CR>")
