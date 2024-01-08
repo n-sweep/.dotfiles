8b465044ee4e2c27e631bc8766e7aaddb06c0e80
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-08 10:10:32 -0500

    cleanup

--- nvim/.config/nvim/lua/pvserv/server/run.py
+++ nvim/.config/nvim/lua/pvserv/server/run.py
@@ -8,26 +8,35 @@ import threading
 import webbrowser
 
 
+def ensure_dir(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+
+def launch_server(host, port, config):
+    app.config.update(config)
+    threading.Timer(0.5, webbrowser.open_new, [app.config['url']]).start()
+    socketio.run(app, host=host, port=port)
+
+
 def main():
+    plot_file = '/tmp/vis/plot.json'
+    log_file = os.path.expanduser('~/.local/share/nvim/pvserv.log')
+
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+
     host_name = socket.getfqdn()
     ip = socket.gethostbyname(host_name)
     port = 5619
 
-    logging.basicConfig(
-        filename=os.path.expanduser('~/.local/share/nvim/pvserv.log'),
-        level=logging.INFO
-    )
-
     config = {
-        'file': '/tmp/vis/plot.json',
+        'file': plot_file,
         'interval': 0.1,
         'url': f'http://{ip}:{port}'
     }
 
-    app.config.update(config)
-
-    threading.Timer(0.5, webbrowser.open_new, [app.config['url']]).start()
-    socketio.run(app, host=ip, port=port)
+    ensure_dir(plot_file)
+    launch_server(ip, port, config)
 
 
 if __name__ == "__main__":
