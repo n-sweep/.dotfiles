e36eabd078abe8394e0e844f4900689cfae206f1
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-09 08:17:45 -0500

    pvserv debugging

--- nvim/.config/nvim/lua/pvserv/server/run.py
+++ nvim/.config/nvim/lua/pvserv/server/run.py
@@ -8,6 +8,16 @@ import threading
 import webbrowser
 
 
+def get_lan_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))  # Use a well-known external server's IP address
+        return s.getsockname()[0]
+
+    except socket.error:
+        return None
+
+
 def ensure_dir(dir):
     if not os.path.isdir(dir):
         os.makedirs(dir)
@@ -16,7 +26,7 @@ def ensure_dir(dir):
 def launch_server(host, port, config):
     app.config.update(config)
     threading.Timer(0.5, webbrowser.open_new, [app.config['url']]).start()
-    socketio.run(app, host=host, port=port)
+    socketio.run(app, host=host, port=port, allow_unsafe_werkzeug=True)
 
 
 def main():
@@ -25,8 +35,7 @@ def main():
 
     logging.basicConfig(filename=log_file, level=logging.INFO)
 
-    host_name = socket.getfqdn()
-    ip = socket.gethostbyname(host_name)
+    ip = get_lan_ip()
     port = 5619
 
     config = {
@@ -35,9 +44,12 @@ def main():
         'url': f'http://{ip}:{port}'
     }
 
-    ensure_dir(plot_file)
+    ensure_dir(os.path.dirname(plot_file))
     launch_server(ip, port, config)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logging.info(e)
