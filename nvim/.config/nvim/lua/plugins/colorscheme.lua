86c3cbd22e9510948aa68ad12993fc59f9f4ad6a
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-15 10:38:40 -0500

    colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,21 @@
-return {
-    "sainnhe/gruvbox-material",
+local P = { "sainnhe/gruvbox-material" }
+
+P.opts = {
     lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd([[colorscheme gruvbox-material]])
-    end,
+    priority = 1000
 }
+
+function P.init()
+    vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_foreground = 'original'
+end
+
+function P.config()
+    vim.cmd([[colorscheme gruvbox-material]])
+
+    -- Transparent background
+    vim.cmd([[hi normal guibg=NONE]])
+
+end
+
+return P
