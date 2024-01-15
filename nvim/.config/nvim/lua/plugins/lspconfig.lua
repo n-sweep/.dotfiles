68a8fd6661d0a548310aae6f347e72919a4ae830
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-15 10:46:46 -0500

    lsp keybinds

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,4 @@
 local P = {"neovim/nvim-lspconfig"}
-local user = {}
 
 P.dependencies = {
     "hrsh7th/cmp-nvim-lsp",
@@ -9,6 +8,14 @@ P.dependencies = {
 
 function P.init()
     vim.diagnostic.config({ virtual_text = true })
+
+    -- Docstring hover
+    vim.keymap.set("n", "<leader>k", "<CMD>lua vim.lsp.buf.hover()<CR>")
+
+    -- Linting hover
+    vim.keymap.set("n", "<leader>ee", "<CMD>lua vim.diagnostic.open_float()<CR>")
+    vim.keymap.set("n", "<leader>en", "<CMD>lua vim.diagnostic.goto_next()<CR>")
+    vim.keymap.set("n", "<leader>eN", "<CMD>lua vim.diagnostic.goto_prev()<CR>")
 end
 
 function P.config()
