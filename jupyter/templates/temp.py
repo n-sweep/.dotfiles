a24d7c5571c8bbf67ef97340458d867049986871
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-18 16:38:50 -0500

    template update w/ plotting

--- jupyter/templates/temp.py
+++ jupyter/templates/temp.py
@@ -6,18 +6,43 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.15.1
+#   kernelspec:
+#       display_name: Python3
+#       language: python
+#       name: Python3
 # ---
 
 # %% [markdown]
 # # Template Notebook
 
 # %%
-import os
 import closedloop.api_private as api
-import closedloop.ds as ds
+import plotly.express as px
+import plotly.io as pio
+import pandas as pd
 
+# plotly setup
+pio.renderers.default = 'notebook'
+pd.options.plotting.backend = 'plotly'
+
+# plot using CL colors
+cl_colors = ['#207DBA', '#F7C31A', '#1fad58', '#7f52e0']
+cl_template = pio.templates['plotly']
+cl_template.layout.colorway = cl_colors
+pio.templates['cl_template'] = cl_template
+pio.templates.default = 'cl_template'
+
+
+def pwrite(fig, plt='/tmp/vis/plot.json'):
+    fig = fig.update_layout(autosize=False)
+    fig.write_json(plt)
+
+
+# %%
 cl = api.ClosedLoopClientPrivate("PROD")
-psId = ""
+psId = "ltc_aco"
+cl.getPersonStore(psId).name
 
+# %%
 # %%
 # %%
