6972ab24d338f2d309b29db779669b61157a376a
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-02-10 14:56:33 -0500

    housekeeping

--- nvim/.config/nvim/lua/user/set.lua
+++ nvim/.config/nvim/lua/user/set.lua
@@ -1,32 +1,33 @@
 local vim = vim
+local opt = vim.opt
 vim.g.mapleader = " "
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
+opt.nu = true
+opt.relativenumber = true
+opt.cursorline = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 --vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.incsearch = true
 
-vim.opt.conceallevel = 1
-vim.opt.scrolloff = 1999
-vim.opt.signcolumn = 'yes'
-vim.opt.colorcolumn = '80'
-vim.opt.mouse = ''
-vim.opt.wrap = false
-vim.opt.errorbells = false
-vim.opt.termguicolors = true
+opt.conceallevel = 1
+opt.scrolloff = 1999
+opt.signcolumn = 'yes'
+opt.colorcolumn = '80'
+opt.mouse = ''
+opt.wrap = false
+opt.errorbells = false
+opt.termguicolors = true
 
 -- Allow formatting of long lines with "gq".
-vim.opt.formatoptions:append('q')
+opt.formatoptions:append('q')
