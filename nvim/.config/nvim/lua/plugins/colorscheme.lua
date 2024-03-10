09a4f5361a10f56255c6d4c5b0a75f04017754bd
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-03-10 14:31:15 -0400

    more colorscheme bs

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,18 @@
 local vim = vim
 local P = {
-    "sainnhe/everforest",
-    "sainnhe/gruvbox-material",
+    -- colorschemes
+    -- "sainnhe/everforest",
+    -- "sainnhe/gruvbox-material",
     "rebelot/kanagawa.nvim",
-    "mellow-theme/mellow.nvim",
-    "sebasruiz09/fizz.nvim",
-    "AlexvZyl/nordic.nvim",
-    "ribru17/bamboo.nvim"
-}
-
-P.opts = {
-    lazy = false,
-    priority = 1000
+    -- "mellow-theme/mellow.nvim",
+    -- "sebasruiz09/fizz.nvim",
+    -- "AlexvZyl/nordic.nvim",
+    -- "ribru17/bamboo.nvim",
 }
 
 function P.config()
-
     -- default color scheme
     vim.cmd([[colorscheme kanagawa]])
-
     -- Transparent background
     -- vim.cmd([[hi normal guibg=NONE]])
 end
