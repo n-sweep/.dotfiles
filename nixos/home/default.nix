5866ff42794a72ae32aa43afac06685154e14732
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-09-01 13:26:02 -0400

    552 current  2024-09-01 09:11:59  24.05.20240827.36bae45  6.6.47                          *

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -367,7 +367,7 @@ in {
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.11";
+  home.stateVersion = "24.05";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
