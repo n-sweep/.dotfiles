3ed16fc7496f5ebc118c1966b398170d92eb6470
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-09-16 08:23:31 -0400

    55 current  2024-09-16 08:23:26  24.11.20240910.1355a0c   6.6.50                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -23,6 +23,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser = {
+      url = "github:MarceColl/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
