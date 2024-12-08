6ceeaaa2b4b65b434a91b3f7e1905574c570f2cb
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-12-08 06:59:40 -0500

    nix flake sha updating

---
+++ nixos/modules/scnvim/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for scnvim";
+
+  outputs = { self, nixpkgs }: {
+    # Define your packages or configurations
+    packages.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux ;
+    in pkgs.stdenv.mkDerivation {
+      pname = "scnvim";
+      version = "1.0";
+
+      src = pkgs.fetchgit {
+        url = "https://github.com/davidgranstrom/scnvim";
+        sha256 = "sha256-euHwoK2WHLF/hrjLY2P4yGrIbYyBN38FL3q4CKNZmLY=";
+      };
+
+      installPhase = ''
+        mkdir -p "$out"
+        cp -r "$src"/* "$out"
+      '';
+
+    };
+  };
+}
