7ca83fbe59a536c57304871b174f1d7ad6b2cb10
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-02-09 08:12:59 -0500

    ipython gitignore

--- .ipython/profile_default/startup/00.py
+++ .ipython/profile_default/startup/00.py
@@ -1,9 +1,24 @@
-import os
 import requests
 import socket
 import pandas as pd
 import plotly.io as pio
 import plotly.graph_objects as go
+
+# pandas setup
+pd.options.plotting.backend = 'plotly'
+
+# plotly setup
+pio.templates.default = "plotly_dark"
+
+
+# overriding plotly's Figure.show()
+_show = go.Figure.show
+def modified_show(self, *args, **kwargs) -> None:
+    r = requests.get(f'http://{get_lan_ip()}:5619/plot', json=self.to_json())
+    if r.status_code != 200:
+        _show(self, *args, **kwargs)
+go.Figure.show = modified_show
+
 
 def get_lan_ip() -> None:
     """get the LAN IP address of the machine running the server"""
@@ -14,21 +29,9 @@ def get_lan_ip() -> None:
     except socket.error:
         return None
 
-# pandas setup
-pd.options.plotting.backend = 'plotly'
-
-# plotly setup
-pio.templates.default = "plotly_dark"
-
-# overriding plotly's Figure.show()
-_show = go.Figure.show
-def modified_show(self, *args, **kwargs) -> None:
-    r = requests.get(f'http://{get_lan_ip()}:5619/plot', json=self.to_json())
-    if r.status_code != 200:
-        _show(self, *args, **kwargs)
-go.Figure.show = modified_show
 
 def show_df(df: pd.DataFrame, updates: dict = {}) -> None:
+    """plots a table of a pandas dataframe in plotly"""
     fig = go.Figure(data=go.Table(
        header=dict(values=df.columns),
         cells=dict(values=[df[col] for col in df.columns]),
