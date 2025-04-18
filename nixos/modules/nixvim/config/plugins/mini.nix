f864a39de80f8c2ee233e61673f5df406d343b78
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-04-18 10:39:39 -0400

    mini.diff avoiding ipynb files

--- nixos/modules/nixvim/config/plugins/mini.nix
+++ nixos/modules/nixvim/config/plugins/mini.nix
@@ -10,4 +10,19 @@
       };
     };
 
+    autoCmd = [
+
+      {
+        desc = "mini.diff: ignore ipynb files";
+        event = "FileType";
+        pattern = "ipynb";
+        callback = { __raw = ''
+          function (args)
+            vim.b[args.buf].minidiff_disable = true
+          end
+        ''; };
+      }
+
+    ];
+
 }
