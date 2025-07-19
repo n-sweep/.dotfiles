738c7d0f00eb4f7c79cb5d868d645032e7ab7218
n-sweep <34486798+n-sweep@users.noreply.github.com>
2025-07-19 08:21:08 -0400

    tmux pane swapping fix

--- nixos/modules/nixvim/config/plugins/lua/tmux.lua
+++ nixos/modules/nixvim/config/plugins/lua/tmux.lua
@@ -12,6 +12,12 @@ vim.keymap.set("n", "<C-M-j>", function() tmux.resize_bottom() end)
 vim.keymap.set("n", "<C-M-k>", function() tmux.resize_top() end)
 vim.keymap.set("n", "<C-M-l>", function() tmux.resize_right() end)
 
+-- vim & tmux split swapping
+vim.keymap.set("n", "<S-M-h>", function() tmux.swap_left() end)
+vim.keymap.set("n", "<S-M-j>", function() tmux.swap_bottom() end)
+vim.keymap.set("n", "<S-M-k>", function() tmux.swap_top() end)
+vim.keymap.set("n", "<S-M-l>", function() tmux.swap_right() end)
+
 tmux.setup({
     copy_sync = {enable = false},
     navigation = {
@@ -19,4 +25,5 @@ tmux.setup({
         enable_default_keybindings = false,
     },
     resize = {enable_default_keybindings = false},
+    swap = {enable_default_keybindings = false},
 })
